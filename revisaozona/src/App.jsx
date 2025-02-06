6e87e95ca91096253697c906d576624cb5e2f800
Tati6630 <tati78marquesd@gmail.com>
2025-02-05 22:10:46 -0300

    criando 1ºs passos do projeto

--- revisaozona/src/App.jsx
+++ revisaozona/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Header from './components/Header'
+import Body from './components/Body/Body.jsx'
+import Header from './components/Header/Header.jsx'
 
 function App() {
   
 
   return (
     <>
-    <Header ></Header>
+    <Header />
+    <Body />
     </>
     )
 }
---
+++ revisaozona/src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='container-body'>
+        <h1>Página importante para o mundo</h1>
+        </div>
+  )
+}
+
+export default Body
